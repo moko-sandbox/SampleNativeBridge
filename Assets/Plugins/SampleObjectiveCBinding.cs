386811f84b2bce9f2c261386834ac6280aa9fc5f
moko-ist <yukie.hr@gmail.com>
2018-04-24 15:05:20 +0900

    Add bridge for native objective-c code

---
+++ Assets/Plugins/SampleObjectiveCBinding.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Runtime.InteropServices;
+
+public class Binding {
+    [DllImport("__Internal")]
+    private static extern void test_ ();
+
+	public static void test () {
+		if (Application.platform != RuntimePlatform.OSXEditor) {
+        	test_();
+		}
+    }
+}
---
+++ Assets/Plugins/iOS/SampleObjectiveC.mm
@@ -0,0 +1,7 @@
+#import <Foundation/Foundation.h>
+
+extern "C"{
+    void test_() {
+        NSLog(@"[Objective-C] Called Objective-C method :-)");
+    }
+}
