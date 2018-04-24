5fcef658ef8c655151ae7cbe5f9ad3d75b0486e2
moko-ist <yukie.hr@gmail.com>
2018-04-24 15:22:35 +0900

    Add button to call objective-c method

---
+++ Assets/Scripts/CallObjectiveCButton.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CallObjectiveCButton : MonoBehaviour {
+	public void PressObjectiveCButton () {
+		Binding.test();
+	}
+}
