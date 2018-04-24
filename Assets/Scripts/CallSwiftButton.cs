a16fcb101ec5854b17c80bf93fe71063f3a9e784
moko-ist <yukie.hr@gmail.com>
2018-04-24 18:15:54 +0900

    Add button to call swift method

---
+++ Assets/Scripts/CallSwiftButton.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CallSwiftButton : MonoBehaviour {
+    public void PressSwiftButton()
+    {
+        BindingSwift.callSwiftMethod();
+    }
+}
